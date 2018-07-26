d7582b273e843e27abf00d95c41baf7dfadc0875
James Rock <hello@jamesrock.me>
2018-07-26 18:39:27 +0100

    adds sounds to worker

--- js/worker.js
+++ js/worker.js
@@ -2,11 +2,15 @@ self.addEventListener('install', function(event) {
 	event.waitUntil(
 		caches.open('v1').then(function(cache) {
 			return cache.addAll([
-				'/audio/',
+				'/audio/die.mp3',
+				'/audio/hit.mp3',
+				'/audio/point.mp3',
+				'/audio/swoosh.mp3',
+				'/audio/wing.mp3',
 				'/index.html',
 				'/spritesheet.png',
 				'/js/frappybird.js',
-				'/css/frappybird.css',
+				'/css/frappybird.css'
 			]);
 		})
 	);
